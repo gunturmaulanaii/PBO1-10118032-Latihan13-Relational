4a76bc552422b3a11cea61ce5aa5c8628bc578c7
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-09-25 16:26:46 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan13-Relational/src/latihan/pkg13/relational/PBO110118032Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg13.relational;
+
+/**
+ *
+ * @author
+ * NAMA  : Guntur Maulana Ibrahim
+ * Kelas : PBO1
+ * NIM   : 10118032
+ * Deskripsi Program : Program ini berisi untuk menampilkan tipe data relasional dan tipe data boolean
+ */
+public class PBO110118032Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " +(bil1==bil2));
+        System.out.println("a != b = " +(bil1!=bil2));
+        System.out.println("a > b = " + (bil1>bil2));
+        System.out.println("a < b = " +(bil1<bil2));
+        System.out.println("b >= a = " +(bil2>=bil1));
+        System.out.println("b <= a = " +(bil2<=bil1));
+    }
+    
+}
